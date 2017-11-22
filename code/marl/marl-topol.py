3d2c10aba7bd98e9e221cf4c9445562b265241a3
Kyle Simpson <kyleandrew.simpson@gmail.com>
2017-11-22 11:21:55 +0000

    [MARL] Fix distinct lack of routing.

--- code/marl/marl-topol.py
+++ code/marl/marl-topol.py
@@ -5,6 +5,8 @@ from mininet.cli import CLI
 from mininet.net import Mininet
 from mininet.clean import Cleanup
 
+linkopts = {"bw": 10}
+
 # Maaaaan forget all of that for now.
 
 # class MarlTest(Topo):
@@ -20,20 +22,26 @@ from mininet.clean import Cleanup
 
 Cleanup.cleanup()
 
-BWLIMIT = 10
+net = Mininet(link=TCLink)#, autoStaticArp=True)
 
-net = Mininet(link=TCLink, autoStaticArp=True)
+# Turns out this is real important. Whoooops...
+net.addController()
 
 # Okay, switches exist and have a tree like topology (s1 is the "head")
 # Suppose for now hat they all have the same (low) bandwidth.
 switches = [net.addSwitch("s{}".format(i)) for i in xrange(3)]
 hosts = [net.addHost("h{}".format(i)) for i in xrange(3)]
+links = []
+
+def trackedLink(src, target):
+	links.append(net.addLink(src, target, **linkopts))
+	print "New link between {} and {}!".format(src.name, target.name)
 
 def maybeLink(target):
 	if target <= len(switches):
 		si = int(target/2)-1
 		ti = target-1
-		net.addLink(switches[si], switches[ti], bw=BWLIMIT)
+		trackedLink(switches[si], switches[ti])
 
 for i, el in enumerate(switches):
 	ti = i + 1
@@ -42,18 +50,22 @@ for i, el in enumerate(switches):
 
 # Fix this later to target head, then leaves.
 for i in xrange(3):
-	net.addLink(hosts[i], switches[i], bw=BWLIMIT)
+	trackedLink(hosts[i], switches[i])
+
+print links
 
 net.start()
 
-net.pingAll()
+net.waitConnected()
+
+# net.pingAll()
 
 # print hosts[0].cmd("iperf")
 
 for i, sw in enumerate(switches):
-	sw.sendCmd("python", "agent.py", i, BWLIMIT, "|", "{}.txt".format(i))
+	sw.sendCmd("python", "agent.py", i, linkopts["bw"], "|", "{}.txt".format(i))
 
-net.iperf((hosts[1],hosts[2]))
+# net.iperf((hosts[1],hosts[2]))
 
 CLI(net)
 net.stop()
