7b533ad1c033517f21d0db04fc7585748bc44226
None <None>
2018-01-23 15:23:18 -0800

    [MARL] Fix'd

--- code/marl/marl-topol.py
+++ code/marl/marl-topol.py
@@ -184,7 +184,7 @@ def updateUpstreamRoute(switch, out_port=1, ac_prob=0.0):
 	]
 
 	# Try building that message from scratch, here.
-	msg = flow_pdrop_msg[:-24] + ofbp._pack("I", p_drop_num) + flow_pdrop_msg[-16:]
+	msg = flow_pdrop_msg[:-20] + ofpb._pack("I", p_drop_num) + flow_pdrop_msg[-16:]
 	#For now
 	#print msg
 	
