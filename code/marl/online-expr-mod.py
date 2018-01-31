e730f807a922290a3c51d77e3cd6ed0c03f379a4
Kyle Simpson <kyleandrew.simpson@gmail.com>
2018-01-31 11:44:34 +0000

    [MARL] New experiment for test purposes.

---
+++ code/marl/online-expr-mod.py
@@ -0,0 +1,27 @@
+from marl import *
+from writer import writeResults, makeResultsAverage
+
+results = marlExperiment(
+	n_teams = 5,
+
+	n_inters = 2,
+	n_learners = 3,
+	host_range = [10, 10],
+
+	explore_episodes = 0.3,
+	episodes = 10,#500, Since mininet keeps running out of files even e/ cleanup
+	episode_length = 10000,
+	separate_episodes = True,
+
+	alpha = 0.05,
+	epsilon = 0.2,
+	discount = 0,
+
+	dt = 0.01,
+
+	rf = "ctl"
+)
+
+writeResults("../../results/online-mod.csv", results)
+
+makeResultsAverage("../../results/online-mod.csv", "../../results/online-mod-avg.csv")
