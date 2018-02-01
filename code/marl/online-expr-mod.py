ab439703bd010abb2d28264cbbeec14da0465617
Kyle Simpson <kyleandrew.simpson@gmail.com>
2018-01-31 17:10:33 -0800

    Slightly neater config for new experiment.

--- code/marl/online-expr-mod.py
+++ code/marl/online-expr-mod.py
@@ -1,12 +1,14 @@
 from marl import *
 from writer import writeResults, makeResultsAverage
 
+hosts_p = 5
+
 results = marlExperiment(
 	n_teams = 5,
 
 	n_inters = 2,
 	n_learners = 3,
-	host_range = [10, 10],
+	host_range = [hosts_p, hosts_p],
 
 	explore_episodes = 0.3,
 	episodes = 10,#500, Since mininet keeps running out of files even e/ cleanup
