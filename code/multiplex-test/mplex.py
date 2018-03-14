30c1e0f188617e7656c865e6d9d93c13f128c89d
Kyle Simpson <kyleandrew.simpson@gmail.com>
2018-03-14 13:35:53 +0000

    Quick experiment to check TCP multiplexing is fair

---
+++ code/multiplex-test/mplex.py
@@ -0,0 +1,263 @@
+from mininet.topo import Topo
+from mininet.node import OVSSwitch, RemoteController
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.clean import Cleanup
+
+import twink.ofp5 as ofp
+import twink.ofp5.build as ofpb
+import twink.ofp5.oxm as ofpm
+import twink.ofp5.parse as ofpp
+
+import itertools
+import numpy as np
+import os
+import random
+import signal
+import socket
+from subprocess import PIPE, Popen
+import sys
+import time
+
+def mplexExperiment(
+		n = 5,
+		inc_function = None,
+
+		pdrop_min = 0.0,
+		pdrop_max = 1.0,
+		pdrop_stride = 0.1,
+
+		playback_file = "../../data/pcaps/bigFlows.pcap",
+		playback = False,
+
+		linkopts = {},
+		base_iperf_port = 5201
+	):
+
+	if inc_function is None:
+		inc_function = lambda last, i, n: float(i)
+
+	# helpers
+
+	initd_host_count = [0]
+	initd_switch_count = [0]
+
+	def newNamedHost(**kw_args):
+		o = net.addHost("h{}".format(initd_host_count[0]), **kw_args)
+		initd_host_count[0] += 1
+		return o
+
+	def newNamedSwitch(**kw_args):
+		o = net.addSwitch("s{}".format(initd_switch_count[0]), listenPort=7000+initd_switch_count[0], **kw_args)
+		initd_switch_count[0] += 1
+		return o
+
+	def trackedLink(src, target, extras=None):
+		if extras is None:
+			extras = linkopts
+		l = net.addLink(src, target, **extras)
+		return l
+
+	route_commands = [[]]
+	switch_sockets = [{}]
+
+	def openSwitchSocket(switch):
+		if switch.name in switch_sockets[0]:
+			killsock(switch_sockets[0][switch.name])
+			
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(("127.0.0.1", switch.listenPort))
+		s.sendall(ofpb.ofp_hello(None, None))
+		ofpp.parse(s.recv(8))
+		switch.control_socket = s
+
+		switch_sockets[0][switch.name] = s
+		return s
+
+	def killsock(s):
+		try:
+			s.shutdown(socket.SHUT_RDWR)
+		except:
+			pass
+		s.close()
+
+	def removeAllSockets():
+		for _, sock in switch_sockets[0].viewitems():
+			killsock(sock)
+		switch_sockets[0] = {}
+
+	def updateOneRoute(switch, cmd_list, msg):
+		if not switch.listenPort:
+			switch.cmd(*cmd_list)
+		else:
+			s = (switch_sockets[0][switch.name]
+				if switch.name in switch_sockets[0]
+				else openSwitchSocket(switch)
+			)
+
+			# seems like pipes can randomly break. oops!
+			sent = False
+			while not sent:
+				try:
+					s.sendall(msg)
+					sent = True
+				except:
+					s = openSwitchSocket(switch)
+
+	def executeRouteQueue():
+		for el in route_commands[0]:
+			updateOneRoute(*el)
+		route_commands[0] = []
+
+	# Downstream should be computed similarly, but without the probdrops or anything like that.
+	flow_pdrop_msg = ofpb.ofp_flow_mod(
+		None, 0, 0, 0, ofp.OFPFC_ADD,
+		0, 0, 1, None, None, None, 0, 1,
+		ofpb.ofp_match(None, None, None),
+		ofpb.ofp_instruction_actions(ofp.OFPIT_WRITE_ACTIONS, None, [
+			# Looks like 29 is the number I picked for Pdrop.
+			ofpb._pack("HHI", 29, 8, 0xffffffff),
+			ofpb.ofp_action_output(None, 16, 1, 65535)
+		])
+	)
+
+	def updateUpstreamRoute(switch, out_port=1, ac_prob=0.0):
+		# Turn from prob_drop into prob_send!
+		prob = 1 - ac_prob
+		p_drop_num = pdrop(prob)
+		p_drop = "" if ac_prob == 0.0 else "probdrop:{},".format(p_drop_num)
+
+		cmd_tail = [
+			"actions={}\"{}-eth{}\"".format(p_drop, name, out_port)
+		]
+
+		# Try building that message from scratch, here.
+		msg = flow_pdrop_msg[:-20] + ofpb._pack("I", p_drop_num) + flow_pdrop_msg[-16:]
+
+		switchRouteCommand(switch, cmd_tail, msg)
+
+	def updateDownstreamRoute(switch, host, out_port):
+		ip = host.IP()
+
+		cmd_tail = [
+			"nw_dst={}".format(ip),
+			"actions=\"{}-eth{}\"".format(name, out_port)
+		]
+
+		flow_pdrop_msg = ofpb.ofp_flow_mod(
+			None, 0, 0, 0, ofp.OFPFC_ADD,
+			0, 0, 1, None, None, None, 0, 1,
+			ofpb.ofp_match(ofp.OFPMT_OXM, None, [
+				# Need to match Dest IP against curr host's.
+				ofpm.build(None, ofp.OFPXMT_OFB_IPV4_DST, False, 0, socket.inet_aton(ip), None)
+			]),
+			ofpb.ofp_instruction_actions(ofp.OFPIT_WRITE_ACTIONS, None, [
+				# Looks like 29 is the number I picked for Pdrop.
+				ofpb.ofp_action_output(None, 16, out_port, 65535)
+			])
+		)
+
+		switchRouteCommand(switch, cmd_tail, msg)
+
+	def switchRouteCommand(switch, cmd_tail, msg):
+		name = switch.name
+
+		if not switch.listenPort:
+			listenAddr = "unix:/tmp/{}.listen".format(switch.name)
+		else:
+			listenAddr = "tcp:127.0.0.1:{}".format(switch.listenPort)
+
+		cmd_list = [
+			"ovs-ofctl",
+			"add-flow",
+			listenAddr
+		] + cmd_tail
+		
+		if alive:
+			updateOneRoute(switch, cmd_list, msg)
+		else:
+			route_commands[0].append((switch, cmd_list, msg))
+
+	def routedSwitch(upstreamNode, **args):
+		sw = newNamedSwitch(**args)
+		trackedLink(upstreamNode, sw)
+		updateUpstreamRoute(sw)
+		return sw
+
+	def enactActions(learners, sarsas):
+		for (node, sarsa) in zip(learners, sarsas):
+			(_, action, _) = sarsa.last_act
+			updateUpstreamRoute(node, ac_prob=action)
+
+	def buildNet(n_hosts):
+		server = newNamedHost()
+		server_switch = newNamedSwitch()
+
+		core_link = trackedLink(server, server_switch)
+
+		hosts = []
+		last_bw = 0.0
+
+		for i in xrange(n_hosts):
+			h = newNamedHost()
+			bw = inc_function(last_bw, i, n_hosts)
+			last_bw = bw
+
+			trackedLink(server_switch, h)
+
+			hosts.append((h, bw))
+
+			updateDownstreamRoute(server_switch, h, i+2)
+
+		updateUpstreamRoute(server_switch)
+
+		return (server, core_link, server_switch, hosts)
+
+	def pdrop(prob):
+		return int(prob * 0xffffffff)
+
+	def fxrange(start, stop, step):
+		curr = start
+		while curr <= stop:
+			yield curr
+			curr += step
+
+	### THE EXPERIMENT? ###
+
+	net = Mininet(link=TCLink)
+	alive = False
+
+	(server, core_link, server_switch, hosts) = buildNet(n)
+
+	net.start()
+	executeRouteQueue()
+
+	# TODO: Record bandwidth stats
+
+	server_procs = [server.popen(["iperf3", "-s", "-p", str(base_iperf_port+x)], stdin=PIPE, stderr=sys.stderr) for x in xrange(n)]
+	host_procs = []
+
+	for p in fxrange(pdrop_min, pdrop_max, pdrop_stride):
+		updateUpstreamRoute(server_switch, ac_prob=p)
+
+		host_procs = [host.popen(["iperf3", "-p", str(base_iperf_port+x), "-b", "{}M".format(bw)], stdin=PIPE, stderr=sys.stderr) for i, (host, bw) in hosts]
+
+		print "stats for p_drop={:.2f}".format(p)
+		print "=========="
+
+		for proc, (host, bw) in zip(host_procs, hosts):
+			print "bw={}".format(bw)
+			print "----------"
+			print proc.communicate()
+
+	removeAllSockets()
+
+	for proc in host_procs + server_procs:
+		proc.terminate()
+
+	net.stop()
+
+	return None
+
+	mplexExperiment(n=5)
