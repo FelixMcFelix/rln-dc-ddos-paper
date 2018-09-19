bdbd08c51ddc16b867e922316ba3e46a44975896
Kyle Simpson <kyleandrew.simpson@gmail.com>
2018-09-19 16:15:59 +0100

    Now just need to parse files properly and find deps...

--- code/traffic-host/src/walk.rs
+++ code/traffic-host/src/walk.rs
@@ -9,7 +9,7 @@ pub fn walk(config: &Config<'static>) -> io::Result<()> {
 	let root_str = config.http_dir.clone().into_owned();
 	let root = Path::new(&root_str).to_path_buf();
 
-	let mut dirs = vec![root];
+	let mut dirs = vec![root.clone()];
 	let mut files = vec![];
 
 	while !dirs.is_empty() {
@@ -24,15 +24,26 @@ pub fn walk(config: &Config<'static>) -> io::Result<()> {
 			let element = element?;
 			let path = element.path();
 
-			println!("{:?}", path);
+			// println!("{:?}", path);
 
 			if path.is_dir() {
 				dirs.push(path);
 			} else {
-				files.push(element);
+				files.push(path);
 			}
 		}
 	}
+
+	while !files.is_empty() {
+		let curr = {
+			files.pop()
+				.expect("Known to be non-empty.")
+		};
+
+		println!("{:?}", curr.strip_prefix(&root).expect(""));
+	}
+	// .strip_prefix(root)
+	// .expect("All are children of the root...")
 
 	Ok(())
 }
