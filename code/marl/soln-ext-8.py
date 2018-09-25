0f229927eb188a8257936094ef0309bdaef786b6
Kyle Simpson <kyleandrew.simpson@gmail.com>
2018-09-25 13:28:57 +0100

    Long result #1

---
+++ code/marl/soln-ext-8.py
@@ -0,0 +1,35 @@
+from marl import *
+from writer import writeResults, makeResultsAverage
+
+hosts_p = 8
+
+results = marlExperiment(
+	n_teams = 2,#5,
+
+	n_inters = 2,
+	n_learners = 3,
+	host_range = [hosts_p, hosts_p],
+
+	explore_episodes = 0.8,
+	episodes = 10,#50,#500, Since mininet keeps running out of files even e/ cleanup
+	episode_length = 100000,
+	separate_episodes = True,
+
+	alpha = 0.05,
+	epsilon = 0.2,
+	discount = 0,
+
+	dt = 0.05,
+
+#	old_style=True,
+
+	rf = "ctl",
+	use_controller = True,
+	actions_target_flows = True,
+
+	#estimate_const_limit = True,
+)
+
+writeResults("../../results/soln-ext-8.csv", results)
+
+makeResultsAverage("../../results/soln-ext-8.csv", "../../results/soln-ext-8-avg.csv")
