cf4bcd8412a4f1888cdf30e2050b26adca3e8a23
Kyle Simpson <kyleandrew.simpson@gmail.com>
2018-10-29 13:57:26 +0000

    Making it easier to run more expts...

---
+++ code/marl/purger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+function purgeprocs {
+	ps ax | grep "$1" | awk -F ' ' '{print $1}' | xargs sudo kill -9
+	#echo "$1"
+}
+
+purgeprocs nginx
+purgeprocs hping3
+purgeprocs traffic-host
+purgeprocs tcpreplay-edit
+purgeprocs marl-bwmon
+
